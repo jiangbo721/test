8cd32bb433bb8b48aead520847ef4b6258bcfe55
liujiangbo <jiangbo721@163.com>
2019-07-09 12:15:24 +0800

    code format

--- rebase.py
+++ rebase.py
@@ -4,27 +4,8 @@
 # @file: rebase.py
 # @ide: PyCharm
 # @time: 2019-04-09 15:19
+pylint = 1
 
-master = 1
+flake8 = 2
 
-develop = 1
-
-ma = 2
-
-de = 2
-
-mast = 1
-
-mast = 2
-
-dev = 1
-
-dev = 2
-
-ddd = 3
-
-ddd = 4
-
-mat = 3
-
-matt = 4
+pep8 = 3
